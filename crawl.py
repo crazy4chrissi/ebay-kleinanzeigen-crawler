6cf28f3ae05544147b7b5c7576813ece35693a4f
Christopher Kramer <crazy4chrissi@gmail.com>
2021-06-26 12:46:27 +0100

    Stop crawling when no more pages exist. Removed unused --json-out argument. Compatible with Python <3.9. Reformatting using prettier.

--- crawl.py
+++ crawl.py
@@ -4,7 +4,6 @@ from attrdict import AttrDict
 import mechanicalsoup
 import json
 
-JSON_OUT = 'var/results.json'
 
 def main():
     args = get_args()
@@ -19,29 +18,39 @@ def main():
     for page_num in range(args.page_start, args.page_end + 1):
         url = args.url % ('seite:' + str(page_num))
         if args.verbose:
-            print("\tCrawling page: {:2}/{:2} ({})\n\n".format(page_num, args.page_end, url))
-        results += get_results(browser, url)
+            print(
+                "\tCrawling page: {:2}/{:2} ({})\n\n".format(page_num, args.page_end, url))
+        page = browser.get(url)
+        domain = '/'.join(url.split('/')[0:3])     # Dirty
+        results += get_results(page, domain)
+        if not page.soup.select('.pagination-next'):
+            if args.verbose:
+                print(
+                    "Stop crawling because last seen page does not contain a next-page button, so all results are already fetched")
+            break
 
     print(json.dumps(results, sort_keys=True))
 
 
 def get_args():
     import argparse
-    parser = argparse.ArgumentParser(description='Crawl ebay kleinanzeigen', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('--url', default='https://www.ebay-kleinanzeigen.de/s-saarbruecken/%s/peugeot/k0l382', help='The start url. Must have a [percent-sign]s portion in the url to insert the "options" like the page num, price etc.')
-    parser.add_argument('--page-start', default=1, type=int, help='The page number to start at')
-    parser.add_argument('--page-end', default=10, type=int, help='The page number to end at')
-    parser.add_argument('--json-out', default=JSON_OUT, help='The path for the output json.')
-    parser.add_argument('--options', default='', help='Options for kleinanzeigen. Get from the site. Example: "--options preis:0:20"')
-    parser.add_argument('--verbose', default=False, action=argparse.BooleanOptionalAction)
+    parser = argparse.ArgumentParser(
+        description='Crawl ebay kleinanzeigen', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--url', default='https://www.ebay-kleinanzeigen.de/s-saarbruecken/%s/peugeot/k0l382',
+                        help='The start url. Must have a [percent-sign]s portion in the url to insert the "options" like the page num, price etc.')
+    parser.add_argument('--page-start', default=1, type=int,
+                        help='The page number to start at')
+    parser.add_argument('--page-end', default=10, type=int,
+                        help='The page number to end at (may end before if less results found)')
+    parser.add_argument('--options', default='',
+                        help='Options for kleinanzeigen. Get from the site. Example: "--options preis:0:20"')
+    parser.add_argument('--verbose', default=False, action='store_true')
     args = parser.parse_args()
     args.url = args.url % (args.options + '/%s')
     return args
 
-def get_results(browser, url):
-    page = browser.get(url)
-    # Dirty
-    domain = '/'.join(url.split('/')[0:3])
+
+def get_results(page, domain):
     results = []
     for el in page.soup.select('article.aditem'):
         out = AttrDict()
@@ -54,6 +63,7 @@ def get_results(browser, url):
         out.img = img[0].attrs['data-imgsrc'] if len(img) else None
         results.append(out)
     return results
+
 
 if __name__ == '__main__':
     main()
